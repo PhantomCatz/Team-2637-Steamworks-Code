889f81cbfd190c0eb0633a1e81858c553ea68f2c
Tim <31748179+tiimbiim@users.noreply.github.com>
2018-09-12 15:56:46 -0700

    Update Drive.cpp

--- BeachBlitz0.2b/src/Drive.cpp
+++ BeachBlitz0.2b/src/Drive.cpp
@@ -1,15 +1,16 @@
 /*
- * Drive.cpp
+ * Drive.cpp 
  *
  *  Created on: Aug 19, 2017
  *      Author: Programming
+ *      converted: Matt
  */
 
-#include "Drive.h"
-#include "WPILib.h"
-#include "CANTalon.h"
+import Drive.h;
+import WPILib.h;
+import CANTalon.h;
 
-Drive::Drive()
+public void Drive()
 {
 	enum {CAN0,CAN1,CAN2,CAN3,CAN4,CAN5};
 	Fleft = new CANTalon(CAN0);
@@ -18,35 +19,35 @@ Drive::Drive()
 	Rright = new CANTalon(CAN5);
 
 	driver = new RobotDrive(Rleft,Fleft,Fright,Rright);
-	driver->SetExpiration(50);
-	Rleft->SetSafetyEnabled(false);
-	Fleft->SetSafetyEnabled(false);
-	Rright->SetSafetyEnabled(false);
-	Fright->SetSafetyEnabled(false);
-	Rleft->SetExpiration(30);
-	Fleft->SetExpiration(30);
-	Rright->SetExpiration(30);
-	Fright->SetExpiration(30);
+	driver.SetExpiration(50);
+	Rleft.SetSafetyEnabled(false);
+	Fleft.SetSafetyEnabled(false);
+	Rright.SetSafetyEnabled(false);
+	Fright.SetSafetyEnabled(false);
+	Rleft.SetExpiration(30);
+	Fleft.SetExpiration(30);
+	Rright.SetExpiration(30);
+	Fright.SetExpiration(30);
 }
-void Drive::AutoArcade(float x, float y)
+public void AutoArcade(float x, float y)
 {
-	driver->ArcadeDrive(x, y);
+	driver.ArcadeDrive(x, y);
 }
-void Drive::AutoDrive(float L,float R)
+public void AutoDrive(float L,float R)
 {
-	Rleft->Set(L);
-	Fleft->Set(L);
-	Rright->Set(-R);
-	Fright->Set(-R);
+	Rleft.Set(L);
+	Fleft.Set(L);
+	Rright.Set(-R);
+	Fright.Set(-R);
 }
- void Drive::setDriveControl(XboxController* xbox)
+ public void setDriveControl(XboxController* xbox)
 {
 	//"GTA5/Halo" configuration
 	//driver->ArcadeDrive(-.8*(xbox->GetRawAxis(3)-xbox->GetRawAxis(2)),xbox->GetRawAxis(0));
 
 
 	//"Ben's configuration"
-	 driver->ArcadeDrive(-xbox->GetRawAxis(1),-xbox->GetRawAxis(4));
+	 driver.ArcadeDrive(xbox.GetRawAxis(1), xbox.GetRawAxis(4));
 }
 
 
