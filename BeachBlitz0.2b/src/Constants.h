f0034e4e2102541a98f2e488df409f6cc280b88a
Tim <31748179+tiimbiim@users.noreply.github.com>
2018-09-12 16:04:59 -0700

    Update Constants.h

--- BeachBlitz0.2b/src/Constants.h
+++ BeachBlitz0.2b/src/Constants.h
@@ -3,30 +3,31 @@
  *
  *  Created on: Aug 19, 2017
  *      Author: Programming
+ *       converted: Matt
  */
 
 #ifndef SRC_CONSTANTS_H_
 #define SRC_CONSTANTS_H_
 
 
-	static const int T_RESET = 5;
-	static const double AUTO_SPEED = .47;
+	static final int T_RESET = 5;
+	static final double AUTO_SPEED = .47;
 	enum{BLUE_ALLIANCE,RED_ALLIANCE};
 
 
-	static const bool SOLENOID_CLOSED = false;
-	static const bool SOLENOID_OPEN = true;
+	static final bool SOLENOID_CLOSED = false;
+	static final bool SOLENOID_OPEN = true;
 
 
-	static const double BANGBANG_THRESHOLD = 2;
+	static final double BANGBANG_THRESHOLD = 2;
 	double MIN_POWER = .2;
 	double MAX_POWER = .8;
 	bool testMode = true;
 
 
-	static const double DRIVE_T_OUT = 5.0;
-	static const double TURN_T_OUT = 5.0;
-	static const double TWO_SEC_TOUT = 2.0;
+	static final double DRIVE_T_OUT = 5.0;
+	static final double TURN_T_OUT = 5.0;
+	static final double TWO_SEC_TOUT = 2.0;
 
 	//put similar variables and code together
 
@@ -36,8 +37,8 @@
 	enum {JOYSTICK_TRIGGER=1,SIDE_BUTTON,THUMB_BOTlEFT,THUMB_BOTRIGHT,THUMB_TOPLEFT,THUMB_TOPRIGHT};
 	enum {X_AXIS,Y_AXIS,TWIST_AXIS,SLIDER};
 
-	static const double straightkP = .18;
-	static const double straightkD = .23;  //ORIGINALLY .18
+	static final double straightkP = .18;
+	static final double straightkD = .23;  //ORIGINALLY .18
 
 	double TurnkP = 0.118, TurnkD = 0.174;
 
